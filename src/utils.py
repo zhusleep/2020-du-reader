e9cdfc47eab0cd9844b33e06a66c5a737c2a8612
kaihua <zhuflower@qq.com>
2020-04-10 00:21:07 +0800

    Add files via upload

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import numpy as np
 import torch
+import config
 from redis import StrictRedis
 from torch import nn
 from torch.nn.utils.rnn import pad_sequence
@@ -16,33 +17,51 @@ from torch.utils.data import Dataset
 from transformers import AlbertConfig, AlbertModel, BertTokenizer
 
 
-def convert_one_line(text_a, text_b, tokenizer=None):
-    tokens_a = tokenizer.tokenize(text_a)
-    tokens_b = tokenizer.tokenize(text_b)
+def convert_one_line(item, tokenizer=None, train=True):
+    q_id = item[0]
+    doc_tokens = item[1]
+    query_tokens = item[2]
+    answer = item[3]
+    doc_tokens = doc_tokens.replace(u"“", u"\"")
+    if len(query_tokens) > config.max_query_length:
+        query_tokens = query_tokens[0:config.max_query_length]
     one_token = tokenizer.convert_tokens_to_ids(
-        ["[CLS]"] + tokens_a + ["[SEP]"] + tokens_b + ["[SEP]"])
+        ["[CLS]"] + list(query_tokens) + ["[SEP]"] + list(doc_tokens) + ["[SEP]"])
     token_type = np.zeros(len(one_token))
-    token_type[-len(tokens_b):] = 1
-    return one_token, token_type
+    token_type[-len(doc_tokens)-1:] = 1
+    # start and end position
+    if train:
+        start_pos = item[4]
+        start_id = int(start_pos)
+        start_id += len(query_tokens)+2
+        end_id = start_id + len(answer)
+        assert doc_tokens[start_id: end_id] == answer
+        return q_id, one_token, token_type, start_id,end_id
+    else:
+        return q_id, one_token, token_type
 
 
-class TestDataset(Dataset):
+class ReaderDataset(Dataset):
 
-    def __init__(self, data, vocab_path=None, do_lower=True):
-        super(TestDataset, self).__init__()
+    def __init__(self, data, vocab_path=None, do_lower=True, train=True):
+        super(ReaderDataset, self).__init__()
         self._data = data
-        self._tokenizer = BertTokenizer.from_pretrained(
-            vocab_path, cache_dir=None, do_lower_case=do_lower)
+        self.train = train
+        self._tokenizer = BertTokenizer.from_pretrained('bert-base-chinese', do_lower_case=True)
 
     def __len__(self):
         return len(self._data)
 
     def __getitem__(self, idx):
-        token, token_type = convert_one_line(self._data[idx][0], self._data[idx][1], tokenizer=self._tokenizer)
-        return torch.LongTensor(token), torch.LongTensor(token_type)
+        if self.train:
+            q_id, one_token, token_type, start_id,end_id = convert_one_line(self._data[idx], tokenizer=self._tokenizer)
+            return q_id, torch.LongTensor(one_token), torch.LongTensor(token_type), start_id, end_id
+        else:
+            q_id, one_token, token_type = convert_one_line(self._data[idx], tokenizer=self._tokenizer)
+            return q_id, torch.LongTensor(one_token), torch.LongTensor(token_type)
 
 
-def collate_test_fn(batch):
+def collate_fn(batch):
     token, token_type = zip(*batch)
     token = pad_sequence(token, batch_first=True)
     token_type = pad_sequence(token_type, batch_first=True, padding_value=1)
